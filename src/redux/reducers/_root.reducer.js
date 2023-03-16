4d56fbb77b1443b8ccf174c034230331aca25ca7
Andrea Love <andrealovelovelove@gmail.com>
2023-03-16 16:09:32 -0500

    created route, added route to server, wrote sagas for categories

--- src/redux/reducers/_root.reducer.js
+++ src/redux/reducers/_root.reducer.js
@@ -3,6 +3,7 @@ import errors from './errors.reducer';
 import user from './user.reducer';
 import spots from './spot.reducer';
 import location from './location.reducer';
+import categories from './categories.reducer';
 
 // rootReducer is the primary reducer for our entire project
 // It bundles up all of the other reducers so our project can use them.
@@ -14,7 +15,8 @@ const rootReducer = combineReducers({
   errors, // contains registrationMessage and loginMessage
   user, // will have an id and username if someone is logged in
   spots, //all spots on the map
-  location,
+  location, //location data from LocationMap
+  categories, //categories from database
 });
 
 export default rootReducer;
---
+++ src/redux/reducers/categories.reducer.js
@@ -0,0 +1,11 @@
+//store categories returned from server
+const categoriesReducer = (state = [], action) => {
+    switch (action.type) {
+        case 'SET_CATEGORIES':
+            return action.payload; 
+        default:
+            return state; 
+    }
+}
+
+export default categoriesReducer; 
