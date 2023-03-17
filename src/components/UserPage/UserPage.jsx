7f9d65391bfee7decfb801e1e61591d8fbea2c47
Andrea Love <andrealovelovelove@gmail.com>
2023-03-16 19:29:15 -0500

    wrote map of user data on user page

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import LogOutButton from '../LogOutButton/LogOutButton';
-import {useSelector} from 'react-redux';
-
+import {useSelector, useDispatch} from 'react-redux';
 
 
 function UserPage() {
   // this component doesn't do much to start, just renders some user reducer info to the DOM
   const user = useSelector((store) => store.user);
   // const spotsFromStore = useSelector(store => store.spots);
+  const dispatch = useDispatch(); 
 
       useEffect(() => {
         dispatch({ type: "GET_SPOTS_BY_ID", payload: user.id });
@@ -20,7 +20,18 @@ function UserPage() {
     <div className="container">
       <h2>Welcome, {user.username}!</h2>
       <p>Your ID is: {user.id}</p>
-
+      <ul>
+        {spots.map((spotObject) => {
+          return (
+           <div>
+              <p>{spotObject.description}</p>
+              <p>{spotObject.location.x}</p>
+              <p>{spotObject.location.y}</p>
+           </div>
+          )
+        })}
+        <div></div>
+      </ul>
       <LogOutButton className="btn" />
     </div>
   );
