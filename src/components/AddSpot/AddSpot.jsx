b5934a9af02ecb87b72ca56269c256a0099bfad9
Andrea Love <andrealovelovelove@gmail.com>
2023-03-23 14:40:33 -0500

    added MUI components to AddSpot

--- src/components/AddSpot/AddSpot.jsx
+++ src/components/AddSpot/AddSpot.jsx
@@ -4,6 +4,10 @@ import { useHistory } from 'react-router-dom';
 import LocationMap from '../LocationMap/LocationMap';
 import "./AddSpot.css"
 
+//mui imports 
+import {Box, InputLabel, MenuItem, FormControl, FormHelperText, Select, Stack, TextField, Button } from '@mui/material';
+
+
 function AddSpot () {
     const user = useSelector((store) => store.user);
     const history = useHistory(); 
@@ -41,8 +45,50 @@ function AddSpot () {
     }
 
     return(
-        <div className='addSpotForm'>
-            <form onSubmit={addSpot}>
+        <Box className='addSpotForm' onSubmit={addSpot}>
+             <LocationMap />
+            <Stack spacing={4}>
+                <Stack direction='row' spacing={0}>
+                    <TextField label='Description' variant='outlined'></TextField>
+                    <FormHelperText>Describe where to view this plant - i.e. "When facing the house, it is to the left of the front door by the pine tree"</FormHelperText>
+                </Stack>
+                <Stack direction='row' spacing={0}>
+                </Stack>
+            </Stack>
+            <Box>
+                <FormControl required sx={{ m: 1, minWidth: 120 }}>
+                    <InputLabel id="selectCategory">Category</InputLabel>
+                        <Select
+                            labelId="selectCategory"
+                            id="selectCategory"
+                            value = {newSpot.category}
+                            label="Category *"
+                            onChange={(event) => handleChange(event, 'category')}
+                        >
+                            {categoriesFromStore.map((categoryObject) => (
+                                <MenuItem 
+                                    key={categoryObject.id} 
+                                    value={categoryObject.id}
+                                >
+                                    {categoryObject.name}
+                                </MenuItem>
+                            ))}
+                        </Select>
+                    <FormHelperText>Required</FormHelperText>
+                </FormControl>
+            </Box>
+            <Button type="submit" variant='contained'>
+                Submit
+            </Button>
+            
+        </Box>
+    )
+}
+
+export default AddSpot; 
+
+
+/* <form >
                 <textarea 
                     label='Description' 
                     placeholder='Describe where to view this plant - i.e. "right by the door", or "from the sidewalk facing south, next to the oak tree"' 
@@ -57,11 +103,6 @@ function AddSpot () {
                         </option>
                     ))}
                 </select>
-                 <LocationMap />
+                
                 <button type="submit">Submit</button>
-            </form>
-        </div>
-    )
-}
-
-export default AddSpot; 
+            </form> */
