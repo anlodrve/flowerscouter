c5cde5f3f37cc90e2a95d4caf9aabf70d82979e9
Andrea Love <andrealovelovelove@gmail.com>
2023-03-22 21:07:31 -0500

    got custom icons

--- src/components/Marker/Marker.jsx
+++ src/components/Marker/Marker.jsx
@@ -1,34 +1,53 @@
 import { useState } from "react";
+import { useSelector } from "react-redux";
 
 //google maps imports
-import { MarkerF, InfoWindowF } from "@react-google-maps/api"
+import { MarkerF, InfoWindowF} from "@react-google-maps/api"
 
-function Marker(spotObject) {
-    console.log('spotObject', spotObject.spotObject)
+import peony from './palepink.png'
+import lilac from './palepurple.png'
+import rose from './red.png'
+import sunflower from './yellow.png'
+import iris from './darkpurple.png'
+import tulip from './brightpink.png'
+
+
+function Marker({spotObject}) {
 
     //boolean to show or hide infoWindow
     const [infoWindowOpen, setInfoWindowOpen] = useState(false);
 
     return(
         <>
-            <MarkerF 
-                id={spotObject.spotObject.id}
-                position={({lat: spotObject.spotObject.location.x, lng: spotObject.spotObject.location.y})}
-                onClick={() => setInfoWindowOpen(true)}
-                >
-                    {infoWindowOpen && (
-                        <InfoWindowF 
-                            onCloseClick={() => setInfoWindowOpen(false)}
-                            position={({lat: spotObject.spotObject?.location?.x, lng: spotObject.spotObject?.location?.y})}
+
+        
+                    <MarkerF 
+                        id={spotObject.id}
+                        position={({lat: spotObject.location.x, lng: spotObject.location.y})}
+                        onClick={() => setInfoWindowOpen(true)}
+                        icon={{url:(`${
+                            spotObject.category == 1 ? lilac :
+                            spotObject.category == 2 ? peony :
+                            spotObject.category == 3 ? tulip :
+                            spotObject.category == 4 ? rose :
+                            spotObject.category == 5 ? iris :
+                            spotObject.category == 6 ? sunflower :
+                            ''
+                        }`)}}
                         >
-                            <>
-                                <h3>{spotObject.spotObject.name}</h3>
-                                <p>{spotObject.spotObject.location.x}, {spotObject.spotObject.location.y}</p>
-                                <p>{spotObject.spotObject.description}</p>
-                            </>
-                        </InfoWindowF>
-                     )}
-            </MarkerF>
+                            {infoWindowOpen && (
+                                <InfoWindowF 
+                                    onCloseClick={() => setInfoWindowOpen(false)}
+                                    position={({lat: spotObject?.location?.x, lng: spotObject?.location?.y})}
+                                >
+                                    <>
+                                        <h3>{spotObject.name}</h3>
+                                        <p>{spotObject.location.x}, {spotObject.location.y}</p>
+                                        <p>{spotObject.description}</p>
+                                    </>
+                                </InfoWindowF>
+                             )}
+                    </MarkerF>
         </>
     )
 }
