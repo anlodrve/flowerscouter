f532d70284afaae4463a597938f66d4c70137bdc
Andrea Love <andrealovelovelove@gmail.com>
2023-03-22 23:52:03 -0500

    refactored MainPage with MUI components

--- TODO.md
+++ TODO.md
@@ -1,15 +1,20 @@
 To Do: 
-    [x] U! update location???, categories
-    [] update all maps to center on geolocation
+    [] update add spot map to center on geolocation
+    [] ask dane about adding a spinny wheel 
+    [] update AddSpot to be MUI 
+    [] look up what other "options" props exist for googlemaps
     [] add comments
     [] add likes
     [] nav bar 
     [] shift to mobile view
     [] STYLING 
+    [] Readme!!!!
+    [] double check all the security stuff
     [] filter plants on map by category (category ID?)
     [] STRETCH: show the address of the spot with reverse geocaching 
 
     Done
+        [x] U! update location???, categories (???ish???)
         [x] show map on user page with all their spots     
         [x] figure out the home page???
         [x] add categories 
--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -2,24 +2,35 @@ import React from 'react';
 import MainMap from '../MainMap/MainMap';
 import { useHistory } from 'react-router-dom';
 
+//mui imports
+import { Box, Typography, Button, } from '@mui/material'
+
+
 function HomePage() {
   const history = useHistory(); 
 
-const handleAdd = () => {
-    history.push("/add");
-}
+  const handleAdd = () => {
+      history.push("/add");
+  }
 
-const handleList = () => {
-  history.push("/list");
-}
+  const handleList = () => {
+    history.push("/list");
+  }
 
   return (
-    <div className="container">
-      <p>Home Page</p>
+    <Box 
+      width='350px'
+    >
+      <Typography
+        align='center'
+        variant='h3'
+        >
+          Home Page
+      </Typography>
       <MainMap />
-      <button id="addSpot" onClick={handleAdd}>Add a Spot</button>
-      <button id="toList" onClick={handleList}>List of Spots</button>
-    </div>
+      <Button variant='contained' id="addSpot" onClick={handleAdd}>Add a Spot</Button>
+      <Button variant='contained' id="toList" onClick={handleList}>List of Spots</Button>
+    </Box>
   );
 }
 
