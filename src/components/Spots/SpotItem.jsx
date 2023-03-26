bdd4e9194d41288ab54c395ef897b59aa67bcb75
Andrea Love <andrealovelovelove@gmail.com>
2023-03-26 18:03:47 -0500

    added conditional render for CardMedia photos in SpotItem

--- src/components/Spots/SpotItem.jsx
+++ src/components/Spots/SpotItem.jsx
@@ -10,23 +10,9 @@ import { Box, Card, CardContent, Typography, CardActions, IconButton, CardMedia 
 import { Favorite, FavoriteBorder } from '@mui/icons-material'
 
 
-function SpotItem({spot}) {
+function SpotItem({ spot }) {
 
     const [isLiked, setIsLiked] = useState(false)
-    const categories = useSelector((store) => store.categories)
-
-    // const categoriesKey = () => {
-    //     categories.map((category) => {
-    //         return(
-    //         category.id == 1 ? `https://cdn.shopify.com/s/files/1/1022/5071/products/e93465b598dc4af8a8f39ebf4ff8d64d_f0c4e162-ab1f-4427-8ff6-3a42971efbf2_1800x1800.jpg?v=1676341047` :
-    //         category.id == 2 ? `peony` :
-    //         category.id == 3 ? `tulip` :
-    //         category.id == 4 ? `rose` :
-    //         category.id == 5 ? `iris` :
-    //         category.id == 6 ? `https://www.provenwinners.com/sites/provenwinners.com/files/imagecache/500x500/ifa_upload/helianthus_suncredible_suncredible_yellow.jpg` :
-    //     ''
-    // )})
-    // }
 
     return (
         <Box
@@ -40,13 +26,20 @@ function SpotItem({spot}) {
             >
                 <CardMedia
                     component='img'
-                    height='50'
-                    image={`${`https://cdn.shopify.com/s/files/1/1022/5071/products/e93465b598dc4af8a8f39ebf4ff8d64d_f0c4e162-ab1f-4427-8ff6-3a42971efbf2_1800x1800.jpg?v=1676341047`}`}
+                    height='130'
+                    image={spot.category === 1 ? `${`https://cdn.shopify.com/s/files/1/1022/5071/products/e93465b598dc4af8a8f39ebf4ff8d64d_f0c4e162-ab1f-4427-8ff6-3a42971efbf2_1800x1800.jpg?v=1676341047`}` :
+                        spot.category == 2 ? `${`https://www.gardenia.net/storage/app/public/plant_family/detail/qlnp00IOiv1MpAUEUdDU1VUA10tJCgb0xp7K8Iuk.webp`}` :
+                        spot.category == 3 ? `${`https://images.pexels.com/photos/69776/tulips-bed-colorful-color-69776.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2`}` :
+                        spot.category == 4 ? `${`https://pics.freeartbackgrounds.com/midle/Background_with_Red_Rose_Bush-267.jpg`}` :
+                        spot.category == 5 ? `${`https://www.housedigest.com/img/gallery/how-to-grow-and-take-care-of-a-bearded-iris/how-to-care-for-bearded-iris-1647879768.webp`}` :
+                        spot.category === 6 ? `${`https://bdc2020.o0bc.com/wp-content/uploads/2019/08/sunbelievable-brown-eyed-girl-sunflower-1-6308e983123a4-768x432.jpg?width=900`}` :
+                            ''
+                    }
                     alt='flower image'
                 />
                 <CardActions >
                     {isLiked === true ? (
-                        <IconButton color="primary" aria-label="like button" onClick={() => setIsLiked(false)}>
+                        <IconButton color="primary" aria-label="like button" sx={{}} onClick={() => setIsLiked(false)}>
                             <Favorite />
                         </IconButton>
                     ) : (
